c74d07870a10e90333487ce934adfd7accccf0e1
Ravi Tripathi <ravi.tripathi@go-jek.com>
2020-07-18 19:56:29 +0530

    Initial Commit

---
+++ Shared/GitBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBrowserApp.swift
+//  Shared
+//
+//  Created by Ravi Tripathi on 18/07/20.
+//
+
+import SwiftUI
+
+@main
+struct GitBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
